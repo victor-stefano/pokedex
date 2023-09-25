c82db3fc03d6f240ea80b062f317ea1e10f024fc
Victor Stefano <victorstefanog@gmail.com>
2023-09-25 10:44:36 -0300

    deploy to gh pages

---
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'poke-search',
+  templateUrl: './poke-search.component.html',
+  styleUrls: ['./poke-search.component.scss']
+})
+export class PokeSearchComponent {
+
+  @Output() public emmitSearch: EventEmitter<string> = new EventEmitter();
+
+  public search(value: string){
+    this.emmitSearch.emit(value);
+  }
+}
